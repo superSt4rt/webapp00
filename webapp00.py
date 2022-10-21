5343efc1a8a8e4337f8fb1ebbb8af09ee9c73bfc
superSt4rt <116382078+superSt4rt@users.noreply.github.com>
2022-10-21 16:11:21 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -4,10 +4,10 @@
 import streamlit as st
   
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("SENAI ZERBINI É NOTA 1000!")
+st.title("goku >>>")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("não tenho ideia :p")
+st.header("sim")
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
 st.subheader("Bem vindos!")
 
